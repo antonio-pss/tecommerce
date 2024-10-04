9f5d2822c48545be722dd5658592c2ed5cf03f4e
Antônio Pereira da Silva Santos <79935176+antonio-pss@users.noreply.github.com>
2024-10-04 19:15:33 -0400

    aula 03/10/24

---
+++ tecommerce/teste/models.py
@@ -0,0 +1,94 @@
+from django.db import models
+from django.db.models import Model
+
+
+class ModelBase(models.Model):
+    id = models.BigAutoField(
+        db_column='id',
+        null=False,
+        primary_key=True,
+    )
+
+    active = models.BooleanField(
+        db_column='cs_active',
+        default=True,
+        null=False,
+    )
+
+    created_at = models.DateTimeField(
+        db_column='dt_created_at',
+        auto_now_add=True,
+        null=False
+    )
+
+    modified_at = models.DateTimeField(
+        db_column='dt_created_at',
+        auto_now=True,
+        null=False
+    )
+
+    class Meta:
+        abstract = True
+        managed = True
+
+class Client(ModelBase):
+    name = models.CharField(
+        db_column='tx_name',
+        max_length=70,
+        null=False
+    )
+
+    age = models.IntegerField(
+        db_column='nb_age',
+        null=False,
+    )
+
+    rg = models.CharField(
+        db_column='tx_rg',
+        max_length=12,
+        null=False,
+    )
+
+    cpf = models.CharField(
+        db_column='tx_cpf',
+        max_length=12,
+        null=False,
+    )
+
+class Product(ModelBase):
+    description = models.CharField(
+        db_column='tx_description',
+        null=False
+    )
+
+    quantity = models.IntegerField(
+        db_column='nb_quantity',
+        null=False,
+        default=0,
+    )
+
+class Employee(ModelBase):
+    name = models.CharField(
+        db_column='tx_name',
+        max_length=70,
+        null=False,
+    )
+
+    registration = models.CharField(
+        db_column='tx_registration',
+        max_length=15,
+        null=False,
+    )
+
+class Sale(ModelBase):
+    nrf = models.CharField(
+        db_column='tx_sale',
+        null=False,
+        max_length=255,
+    )
+
+    id_client = models.ForeignKey(Client, on_delete=models.PROTECT)
+
+    id_product = models.ForeignKey(Product, on_delete=models.PROTECT)
+
+    id_employee = models.ForeignKey(Employee, on_delete=models.PROTECT)
